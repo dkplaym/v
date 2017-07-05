66af101b21a1f8cf6d17d035bd5c9e725f0fa05d
dkplaym <dkonex@gmail.com>
2017-07-05 23:22:41 +0800

    fix param bug & add reboot

--- checkproc.sh
+++ checkproc.sh
@@ -11,18 +11,41 @@ if [ "$(whoami | grep root)" = "" ] ; then
     exit 1;
 fi
 
+i=0
+for var in $*
+do
+	i=`expr $i + 1`;
+	if  [ $i -gt 2 ]
+	then
+		cmd=$cmd" "$var	
+   	fi
+done
 
+count=`expr 86400 /2 / $1 `
+#count=4
+i=0
 while true                           
-do         
-
+do 
+	echo $i
+	i=`expr $i + 1`;
+	if [ $i -gt $count ]
+	then
+		
+		echo $i
+		i=0
+		killall $2
+		
+	fi
+        
         result=`ps aux | grep $2 | grep -v checkproc | grep -v grep`
-        echo $result
+        echo "______________" $result "___________"
 	if [ "$result" != "" ]                            
         then                                           
                 echo "True"     
         else               
                 echo "False"   
-		$3
+		echo $cmd
+		$cmd &
         fi     
 	
 	sleep $1
