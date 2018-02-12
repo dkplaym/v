d4bc79cac0c9b91da919eec5805a1a0b3e20d5c9
dkyang <dkonex@gmail.com>
2018-02-12 11:55:39 +0800

    update

--- setup.sh
+++ setup.sh
@@ -19,6 +19,12 @@ bakpath(){
         fi      
 }
 
+writeConfig(){
+	if [[ "$( ls -l $1  2>&1 | grep "No such file" )" == "" ]]  ; then cp  $1 `bakpath` ;   fi 
+	echo "$2"  > $1
+	echo -e "\n\n## by dk -- `date  +%y_%m_%d_%H_%M_%S` "  >> $1  
+}
+
 getgitfile(){
 	lsres=$( ls -l $1 2>&1 | grep "No such file" )
 	if [ "$lsres" != "" ] ; then
@@ -59,84 +65,55 @@ encfile(){
 	openssl des-cbc -in $DEC -out /tools/encinfo.bin  -pass pass:$REPLY ;	
 }
 
-write_dnsmasq(){
-        F="/etc/dnsmasq.conf"
-        result=$( cat $F | grep $MARK )
-                if [[ "$result" != "" ]]
-                then
-                        echo "ignore $F"
-                        return;
-                fi
+append_sendmail(){
 
-        cp  $F `bakpath`
-    	echo '
-no-resolv
-interface=brlan
-bind-interfaces
-dhcp-range=192.168.0.150,192.168.0.200,8760h
+	mkdir -p $MAILDIR
+	chmod 777 $MAILDIR
 
-all-servers
-server=127.0.0.1#9396
-server=127.0.0.1#9397
-cache-size=1500
-max-cache-ttl=3600
-conf-file=/tools/domain.txt
-        ' > $F
-	echo -e "\n\n#$MARK" >> $F
+	lsres=$(ls -l $1  2>&1 | grep "No such file")
+	if [ "$lsres" != "" ] ;then 
+		echo "check file $1 __________  $lsres"
+		return
+	fi		
+	result=$(cat $1 | grep $THISFILE )
+	if [ "$result" != "" ] ; then
+		echo -e "ignore $1  ____________ $result \n\n"
+	fi	
+	cp $1 `bakpath`
+	echo -e "\n\n" >> $1
+	echo  $2 >> $1
 }
 
-setup_client(){
-	getencfile 
+#####################################################################################################################################################################
+#####################################################################################################################################################################
 
-	old=`cat CurrVpsAddr.txt`       
-	echo $old
-	echo $old >> OldVpsAddr.txt
-	echo $1 > CurrVpsAddr.txt
-	cp  /etc/rc.local  /tmp/rc.local
-	sed "s/$old/$1/" /tmp/rc.local > /etc/rc.local
+
+setup_pptp(){
+	if [[ "$(apt list --installed 2>&1  | grep pptpd)" == "" ]] ;	then apt-get -y install pptpd  ;fi
+
+	writeConfig "/etc/ppp/chap-secrets" "$ENC_CHAP" 
+
+	writeConfig "/etc/ppp/pptpd-options" '
+ms-dns 8.8.8.8
+ms-dns 8.8.4.4
+debug
+logfile /tmp/pptpd.log'
 	
-	getgitfile /tools/ss-redir https://github.com/dkplaym/v/raw/master/ss-redir
-	killall -9 ss-redir 
-	nohup /tools/ss-redir -s $1 -b 0.0.0.0 -p 9393 -l 9394 -k $ENC_SSKEY -m aes-256-cfb >/dev/null 2>&1 &
-	getgitfile /tools/ss-local https://github.com/dkplaym/v/raw/master/ss-local
-	killall -9 ss-local
-	nohup /tools/ss-local -s $1 -b 0.0.0.0 -p 9393 -l 9395 -k $ENC_SSKEY -m aes-256-cfb >/dev/null 2>&1 &
-	getgitfile /tools/ss-tunnel https://github.com/dkplaym/v/raw/master/ss-tunnel
-	killall -9 ss-tunnel	
-	nohup /tools/ss-tunnel -u -s $1 -b 0.0.0.0  -p 9393 -l 9396 -L 8.8.8.8:53 -k $ENC_SSKEY -m aes-256-cfb  2>&1 &
-	nohup /tools/ss-tunnel -u -s $1 -b 0.0.0.0  -p 9393 -l 9397 -L 208.67.222.222:53 -k $ENC_SSKEY -m aes-256-cfb  2>&1 &
-	rm /tools/domain.txt
-	cat /tools/domain_raw.txt | tr -d '\r' | grep -v '^$' | sort  | uniq  | while read line   
-	do
-	    echo ipset=/$line/gfwredir >> /tools/domain.txt    
-	    echo server=/$line/127.0.0.1#53 >> /tools/domain.txt
-	done
+	writeConfig "/etc/pptpd.conf"  '
+option /etc/ppp/pptpd-options
+debug
+localip 192.168.10.1
+remoteip 192.168.10.234-238,192.168.10.245  '
 
-	write_dnsmasq
-	service dnsmasq restart
-
-	ipset flush
-
-	echo -e "\n\n\n //todo ____________________________________________"
-
-	echo " rm -fr $HOME/.ssh/id_rsa ; /usr/bin/ssh-keygen -q  -t rsa  -f ~/.ssh/id_rsa -P ''  "
-	echo " ssh-copy-id -i ~/.ssh/id_rsa.pub -p 32132  root@$1"
-	echo "ls -l $HOME/.ssh/ :"
-	ls -l $HOME/.ssh/
-	echo -e "\n\n\n"
- 
+	service pptpd restart
 }
 
-write_name_options(){
-	F="/etc/bind/named.conf.options"
-	result=$( cat $F | grep $MARK )
-		if [[ "$result" != "" ]]
-		then
-			echo "ignore $F"
-			return;
-		fi
-	cp  $F `bakpath`
-	echo '
+
+
+setup_named(){
+	if [[ "$(apt list --installed 2>&1  | grep bind9)" == "" ]] ;	then apt-get -y install bind9  ;fi
+
+	writeConfig "/etc/bind/named.conf.options" '
 options {
      directory "/var/cache/bind";
      forwarders {
@@ -148,23 +125,24 @@ options {
      recursion yes;
      auth-nxdomain no;    # conform to RFC1035
      listen-on port 53553 {127.0.0.1;};
-};
-	
-	' > $F
-echo -e "\n\n#$MARK" >> $F
+};	
+' 
+	service bind9 restart
 }
 
-write_ipsec_conf(){
-        F="/etc/ipsec.conf"
-        result=$( cat $F | grep $MARK )
-                if [[ "$result" != "" ]]
-                then    
-                        echo "ignore $F"
-                        return;
-                fi
-        
-        cp  $F `bakpath`
-        echo '
+
+
+
+
+setup_xl2tp(){
+	if [[ "$(apt list --installed 2>&1  | grep strongswan)" == "" ]] ;	then apt-get -y install strongswan  ;fi
+	if [[ "$(apt list --installed 2>&1  | grep xl2tpd)" == "" ]] ;	then apt-get -y install xl2tpd  ;fi
+
+    writeConfig "/etc/ppp/chap-secrets" "$ENC_CHAP"
+
+	writeConfig "/etc/ipsec.secrets"  ": PSK \"$ENC_PSK\" "
+
+	writeConfig "/etc/ipsec.conf" '
 config setup
    uniqueids=never             
 
@@ -181,22 +159,9 @@ conn L2TP-PSK
     leftfirewall=no
     rightprotoport=17/%any
     type=transport
-    auto=add
-        ' > $F
-echo -e "\n\n#$MARK" >> $F
-}
+    auto=add '
 
-write_xl2tpd_conf(){
-        F="/etc/xl2tpd/xl2tpd.conf"
-        result=$( cat $F | grep $MARK )
-                if [[ "$result" != "" ]]
-                then    
-                        echo "ignore $F"
-                        return;
-                fi
-        
-        cp  $F `bakpath`
-        echo '
+ 	writeConfig  "/etc/xl2tpd/xl2tpd.conf" '
 [global]
 port = 1701
 access control = no
@@ -208,22 +173,10 @@ pppoptfile = /etc/ppp/options.xl2tpd
 ppp debug = yes
 require chap = yes
 refuse pap = yes
-require authentication = yes
-        ' > $F
-echo -e "\n\n;$MARK" >> $F
-}
+require authentication = yes ' 
+        
 
-write_xl2tpd_options(){
-        F="/etc/ppp/options.xl2tpd"
-        result=$( cat $F | grep $MARK )
-                if [[ "$result" != "" ]]
-                then
-                        echo "ignore $F"
-                        return;
-                fi
-
-        cp  $F `bakpath`
-        echo '
+	writeConfig "/etc/ppp/options.xl2tpd" '
 noccp
 auth
 crtscts
@@ -234,76 +187,10 @@ lock
 proxyarp
 silent
 ms-dns 8.8.8.8
-ms-dns 8.8.4.4
-        ' > $F
-echo -e "\n\n#$MARK" >> $F
-}
-write_pptpd_options(){
-        F="/etc/ppp/pptpd-options"
-        result=$( cat $F | grep $MARK )
-                if [[ "$result" != "" ]]
-                then
-                        echo "ignore $F"
-                        return;
-                fi
+ms-dns 8.8.4.4 '
 
-        cp  $F `bakpath`
-        echo '
-ms-dns 8.8.8.8
-ms-dns 8.8.4.4
-debug
-logfile /tmp/pptpd.log'  
-	>> $F
-	echo -e "\n\n#$MARK" >> $F
-}
-
-write_pptpd_conf(){
-        F="/etc/pptpd.conf"
-        result=$( cat $F | grep $MARK )
-                if [[ "$result" != "" ]]
-                then
-                        echo "ignore $F"
-                        return;
-                fi
-
-        cp  $F  `bakpath`
-        echo '
-option /etc/ppp/pptpd-options
-debug
-localip 192.168.10.1
-remoteip 192.168.10.234-238,192.168.10.245
-	' >>  $F
-	echo -e "\n\n#$MARK" >> $F
-}
-
-write_ipsec_secerts(){
-        F="/etc/ipsec.secrets"
-        result=$( cat $F | grep $MARK )
-                if [[ "$result" != "" ]]
-                then
-                        echo "ignore $F"
-                        return;
-                fi
-
-        cp  $F `bakpath`
-        echo ": PSK \"$ENC_PSK\" "   > $F
-        echo -e "\n\n#$MARK" >> $F
-}
-
-write_chap_secrets(){
-        F="/etc/ppp/chap-secrets"
-        result=$( cat $F | grep $MARK )
-                if [[ "$result" != "" ]]
-                then
-                        echo "ignore $F"
-                        return;
-                fi
-
-        cp  $F `bakpath`
-        echo -e "
-$ENC_CHAP
-	"> $F
-echo -e "\n\n#$MARK" >> $F
+	append_sendmail /etc/ppp/ip-up   "/tools/$THISFILE mail on \$PEERNAME"
+	append_sendmail /etc/ppp/ip-down "/tools/$THISFILE mail off \$PEERNAME"
 }
 
 runservice()
@@ -311,99 +198,52 @@ runservice()
     sysctl -w net.ipv4.ip_forward=1
     sysctl -w net.ipv6.conf.all.disable_ipv6=1
 
-	result=$(iptables -L -t  nat -v -n  | grep MASQUE)
-    if [[ "$result" != "" ]]
-    then    
-            echo $result 
-            echo "ignore run iptables"
-    else    
-    e=$(head -n 1 /tools/interface)        
-        iptables -t nat -A POSTROUTING -s 192.168.10.0/24 -o $e  -j MASQUERADE
+    if [[ "$(iptables -L -t  nat -v -n  | grep MASQUE)" == "" ]] ; then    
+	    iptables -t nat -A POSTROUTING -s 192.168.10.0/24 -o $$(head -n 1 /tools/interface)  -j MASQUERADE
     fi
 
-	getgitfile /tools/ss-server https://github.com/dkplaym/v/raw/master/ss-server
     killall -9 ss-server
     nohup /tools/ss-server -s 127.0.0.1 -p 9393 -k $ENC_SSKEY -m aes-256-cfb  >/dev/null 2>&1   &
-    
-    getgitfile /tools/ktserver https://github.com/dkplaym/v/raw/master/ktserver
+
     killall -9 ktserver
     nohup /tools/ktserver -l :19393 -t 127.0.0.1:9393 --crypt none --mtu 1200 --nocomp --mode fast2 --dscp 46 > /dev/null 2>&1 &
 
     service strongswan restart
     service xl2tpd restart
-    #service bind9 restart
-}
-
-append_sendmail(){
-	lsres=$(ls -l $1  2>&1 | grep "No such file")
-	if [ "$lsres" != "" ] ;then 
-		echo "check file $1 __________  $lsres"
-		return
-	fi		
-	result=$(cat $1 | grep $THISFILE )
-	if [ "$result" != "" ] ; then
-		echo -e "ignore $1  ____________ $result \n\n"
-	fi	
-	cp $1 `bakpath`
-	echo -e "\n\n" >> $1
-	echo  $2 >> $1
 }
 
 setup_vps(){
-	cp /usr/share/zoneinfo/Asia/Shanghai  /etc/localtime    # ntpdate time.windows.com
-	getencfile
-	
+	cp /usr/share/zoneinfo/Asia/Shanghai  /etc/localtime    # ntpdate time.windows.com	
 	echo $2 >> /tools/interface
 
-	if [ "$(cat /etc/hostname | grep "^$1")" != "" ] ; then echo "ignore hostname"
-	else echo "$1" > /etc/hostname
-	fi
-	if [ "$(cat /etc/hosts | grep "$1")" != "" ] ; then echo "ignore hosts"
-	else echo "\n127.0.0.1 $1 \n" >> /etc/hosts
-	fi
-	if [ "$(apt list --installed 2>&1  | grep strongswan)" != "" ] ; then echo "ignore apt-get"
-	else apt-get update ; apt-get -y install curl unzip lrzsz strongswan xl2tpd #bind9 
-	fi
-	#rm named.conf.options #sed "s/0.0.0.0/$1/" named.conf.options.4zero > named.conf.options #cp -r ./l2tp/etc/* /etc/ #cp named.conf.options /etc/bind/
+	if [ "$(cat /etc/hostname | grep "^$1")" == "" ] ; then echo "$1" > /etc/hostname  ; fi
 
-	#write_name_options
-	write_ipsec_secerts
-	write_ipsec_conf
-	write_xl2tpd_conf
-	write_xl2tpd_options
-	write_chap_secrets
+	if [ "$(cat /etc/hosts | grep "$1")" == "" ] ; then echo "\n127.0.0.1 $1 \n" >> /etc/hosts ; fi
 
-	mkdir -p $MAILDIR
-	chmod 777 $MAILDIR
-	append_sendmail /etc/ppp/ip-up   "/tools/$THISFILE mail on \$PEERNAME"
-	append_sendmail /etc/ppp/ip-down "/tools/$THISFILE mail off \$PEERNAME"
+	apt-get update;
+	if [ "$(apt list --installed 2>&1  | grep curl)" == "" ] ; then apt-get -y install curl ; fi 
+	if [ "$(apt list --installed 2>&1  | grep curl)" == "" ] ; then apt-get -y install wget ; fi 
+	if [ "$(apt list --installed 2>&1  | grep curl)" == "" ] ; then apt-get -y install unzip ; fi 
+	if [ "$(apt list --installed 2>&1  | grep curl)" == "" ] ; then apt-get -y install lrzsz ; fi 
+
+	getencfile
 	append_sendmail $HOME/.bashrc "/tools/$THISFILE mail last"
+	
+	setup_xl2tp
 
-	if [ "$(cat /etc/rc.local | grep $THISFILE)" != "" ] ;then echo "ignore setup rc.local"
-	else
+	if [ "$(cat /etc/rc.local | grep $THISFILE)" == "" ] ; then 
 		cp /etc/rc.local `bakpath`
 		sed "s/^exit 0//" /tools/rc.local.bak  > /etc/rc.local
 		echo -e "/tools/$THISFILE rc.local \nexit 0;\n" >> /etc/rc.local
 		chmod +x /etc/rc.local
 	fi
+
+	getgitfile /tools/ss-server https://github.com/dkplaym/v/raw/master/ss-server
+    getgitfile /tools/ktserver https://github.com/dkplaym/v/raw/master/ktserver
+
 	runservice;
 }
 
-setup_pptp()
-{
-	result=$(apt list --installed 2>&1  | grep pptpd)
-	if [[ "$result" != "" ]]
-	then
-		echo "ignore apt-get pptpd"
-	else
-		apt-get update
-		apt-get -y install pptpd
-	fi
-	write_chap_secrets
-	write_pptpd_options
-	write_pptpd_conf
-	service pptpd restart
-}
 
 curlmail(){
 	curl -s --url "smtps://smtp.gmail.com:465" --ssl-reqd --mail-from "$ENC_FROM_MAIL" --mail-rcpt "$ENC_TO_MAIL" --upload-file $1 --user "$ENC_FROM_USER" --insecure 
@@ -436,8 +276,6 @@ sendmail(){
 		cat $2 >> $MAIL
                 curlmail $MAIL;
         fi
-
-
 #        rm -rf $MAIL
 }
 
